a89c8b1be32b9524bd909c3b81686830e97ecf01
JDegruson <jdegrusondev@gmail.com>
2024-06-10 15:39:59 +0200

    inject logger in constructor

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -1,23 +1,28 @@
 <?php
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Monolog\Handler\StreamHandler;
-use Monolog\Logger;
-
-$logger = new Logger('channel-name');
-
 
 class ContactController extends AbstractController
 {
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     #[Route('/contact', name: 'contact', methods: ['POST', 'OPTIONS'])]
     public function contact(Request $request): Response
     {
-        $logger->pushHandler(new StreamHandler(__DIR__ . '/app.log', Logger::INFO));
+        $this->logger->info('Received contact request');
+
         if ($request->isMethod('OPTIONS')) {
+            $this->logger->info('OPTIONS request received');
             return new Response('', 204);
         }
 
@@ -25,6 +30,13 @@ class ContactController extends AbstractController
         $prenom = $request->request->get('prenom');
         $email = $request->request->get('email');
         $message = $request->request->get('message');
+
+        $this->logger->info('Contact form data', [
+            'nom' => $nom,
+            'prenom' => $prenom,
+            'email' => $email,
+            'message' => $message,
+        ]);
 
         return $this->json([
             'status' => 'success',
